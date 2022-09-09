71a3cfc99ad6678ed0c08b947b594692e9bc97f7
GlendahMoraa <moraaglendah@gmail.com>
2022-09-08 17:24:20 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * printf: prints sent as standard output
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
+
