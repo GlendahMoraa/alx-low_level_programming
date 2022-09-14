afc4b7f86e53f73ee8f823d9971d2dc794c71dbd
GlendahMoraa <moraaglendah@gmail.com>
2022-09-14 04:12:14 -0700

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,8 +10,9 @@ int main(void)
 {
 	char source[] ="_putchar";
 	int i;
+	int len = strlen(source)
 
-	for (i = 0; i < strlen(source); i++)
+	for (i = 0; i < len; i++)
 		_putchar(source[i]);
 	_putchar('\n');
 
